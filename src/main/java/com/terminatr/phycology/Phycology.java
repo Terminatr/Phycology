a8bdbde969bf82f142b74fb15ccdb202ba4d695b
Terminatr <39315999+Terminatr@users.noreply.github.com>
2019-09-28 01:13:01 -0400

    Add main mod class
    Add standardized entry point for main mod class.
    Update resource files to reflect mod info.

---
+++ src/main/java/com/terminatr/phycology/Phycology.java
@@ -0,0 +1,12 @@
+package com.terminatr.phycology;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.terminatr.phycology.Phycology.MOD_ID;
+
+@Mod(MOD_ID)
+public class Phycology {
+
+    public static final String MOD_ID = "phycology";
+}
